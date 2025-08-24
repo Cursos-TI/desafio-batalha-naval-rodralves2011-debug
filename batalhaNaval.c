8e57c025565619e23b71010ada7a17cf200569bf
Felipe-Alves <rodralves2011@gmail.com>
2025-08-24 20:48:07 +0000

    Criei matriz e vetor

--- batalhaNaval.c
+++ batalhaNaval.c
@@ -9,7 +9,41 @@ int main() {
     // Sugestão: Declare uma matriz bidimensional para representar o tabuleiro (Ex: int tabuleiro[5][5];).
     // Sugestão: Posicione dois navios no tabuleiro, um verticalmente e outro horizontalmente.
     // Sugestão: Utilize `printf` para exibir as coordenadas de cada parte dos navios.
+       
+    // Tamanho do tabuleiro
+    const int linhas = 5;
+    const int colunas = 5;
 
+    // Vetores para armazenar coordenadas dos navios
+    int navioVertical [3][2]; // navio com 3 partes (linha, coluna)
+    int navioHorizontal [4][2]; // navio com 4 partes (linha, coluna)
+
+    // Posição dos navios vertical (coluna fixa linha diferente)
+    int colunaV = 2; // coluna fixa
+    int linhaInicialV = 4; // começa na linha 4
+    for (int i = 0; i < 3; i++){
+        navioVertical[i][0] = linhaInicialV + i; // linha
+        navioVertical[i][1] = colunaV; // coluna
+    }
+    //posição do navio horizontal (linha fixa coluna diferente)
+    int linhaH = 3; //linha fixa
+    int colunaInicialH = 5; // começa na coluna 5
+    for (int i = 0; i < 4; i++) {
+        navioHorizontal[i][0] = linhaH;  //linha
+        navioHorizontal[i][1] = colunaInicialH + i; // coluna
+    }
+
+    // Coordenadas dos navios
+    printf("=== Coordenandas dos Navios ===\n")
+
+    printf("\nNavio Vertical ( 3 partes):\n");
+    for (int i = 0; i < 3; i++) {
+        printf("Parte %d -> Linha: %d | coluna: %d\n")
+    }
+
+
+
+    }
     // Nível Aventureiro - Expansão do Tabuleiro e Posicionamento Diagonal
     // Sugestão: Expanda o tabuleiro para uma matriz 10x10.
     // Sugestão: Posicione quatro navios no tabuleiro, incluindo dois na diagonal.
